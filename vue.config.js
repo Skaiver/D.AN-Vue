88aa51013af7eef3c694889a2aea0139515945f6
Malte <xm3lte@gmail.com>
2024-08-23 21:34:17 +0200

    Initial dist subtree commit

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   // ...other vue-cli plugin options...
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/D.AN-Vue/'
-    : '/',
+  publicPath: '/D.AN-Vue/',
   pwa: {
     name: 'My App',
     themeColor: '#4DBA87',
