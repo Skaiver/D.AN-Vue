8f4e38eceb0e245c818e6a3ae095c6a158035783
Malte <xm3lte@gmail.com>
2024-08-23 22:15:28 +0200

    finally found correct config file >:

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,30 @@
+// const path = require('path')
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+/*
+ * 
+ * 
+ *  DO NOT USE THIS FILE FOR CONFIGURATION IN THIS PROJECT!!!
+ * 
+ * 
+/*
+
 module.exports = {
   // ...other vue-cli plugin options...
-  publicPath: '/D.AN-Vue/',
+  // publicPath: '/D.AN-Vue/',
+  // outputDir: 'dist/D.AN-Vue/',
+  // configureWebpack: () => {
+  //   return {
+  //     plugins: [
+  //       new PrerenderSPAPlugin({
+  //         staticDir: path.join(__dirname, 'dist'),
+  //         outputDir: path.join(__dirname, '/dist/D.AN-Vue/'),
+  //         indexPath: path.join(__dirname, 'dist/D.AN-Vue/index.html'),
+  //         routes: ['/D.AN-Vue', '/D.AN-Vue/dashboard', '/D.AN-Vue/export', '/D.AN-Vue/import', '/D.AN-Vue/settings']
+  //       })
+  //     ]
+  //   }
+  // },
   pwa: {
     name: 'My App',
     themeColor: '#4DBA87',
